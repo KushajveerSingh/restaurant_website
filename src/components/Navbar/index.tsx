1e5df64b543e6c679e9793bfb2e676f2c2594832
KushajveerSingh <kushajreal@gmail.com>
2022-10-18 05:50:03 -0400

    completed navbar for all screens and removed images file

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,79 @@
 import styles from './Navbar.module.css';
+import { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
-    <div>
-      <>Navbar</>
-    </div>
+    <nav className={styles.app__navbar}>
+      <div className={styles.app__navbar_logo}>
+        <img src="/gericht.png" alt="app logo" />
+      </div>
+
+      <ul className={styles.app__navbar_links}>
+        <li className="p__opensans">
+          <a href="#home">Home</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#about">About</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#menu">Menu</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#awards">Awards</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className={styles.app__navbar_login}>
+        <a href="#login" className="p__opensans">
+          Log In / Register
+        </a>
+        <div />
+        <a href="/" className="p__opensans">
+          Book Table
+        </a>
+      </div>
+
+      <div className={styles.app__navbar_smallscreen}>
+        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
+
+        {toggleMenu && (
+          <div
+            className={`${styles.app__navbar_smallscreen_overlay} flex__center slide-bottom`}
+          >
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className={styles.overlay__close}
+              onClick={() => setToggleMenu(false)}
+            />
+
+            <ul className={styles.app__navbar_smallscreen_links}>
+              <li className="p__opensans">
+                <a href="#home">Home</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#about">About</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#menu">Menu</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#awards">Awards</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
   );
 };
 
--- src/data/index.ts
+++
@@ -1,5 +0,0 @@
-import images from './images';
-import meal from '/meal.mp4';
-import data from './data';
-
-export { images, meal, data };
