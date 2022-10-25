c97bc186ea9d04f77ca9d4a8901cffd6bc0f0f10
KushajveerSingh <kushajreal@gmail.com>
2022-10-25 04:08:08 -0400

    intro section complete

--- src/containers/Intro/index.tsx
+++ src/containers/Intro/index.tsx
@@ -1,9 +1,44 @@
+import { useRef, useState } from 'react';
 import styles from './Intro.module.css';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 
 const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const videoRef = useRef();
+
+  const handleVideo = () => {
+    setPlayVideo(!playVideo);
+
+    if (playVideo) {
+      videoRef.current.pause();
+    } else {
+      videoRef.current.play();
+    }
+  };
+
   return (
-    <div>
-      <>Intro</>
+    <div className={styles.app__video}>
+      <video
+        src="/meal.mp4"
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+        ref={videoRef}
+      />
+
+      <div className={`${styles.app__video_overlay} flex__center`}>
+        <div
+          className={`${styles.app__video_overlay_circle} flex__center`}
+          onClick={handleVideo}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
